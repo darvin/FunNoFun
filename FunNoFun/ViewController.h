3d34aee16cdc8ddf32f73b2cb8cb4b4db1b9fe2d
Sergey Klimov <sergey.v.klimov@gmail.com>
2012-04-12 11:06:49 -0500

    Initial Commit

---
+++ FunNoFun/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FunNoFun
+//
+//  Created by Sergey Klimov on 4/12/12.
+//  Copyright (c) 2012 Self-Employed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
